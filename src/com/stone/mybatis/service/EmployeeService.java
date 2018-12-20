1e9afc058178ed38925af869a74be0874a7846be
stonebox1122 <287779438@qq.com>
2018-12-20 11:57:00 +0800

    commit

--- src/com/stone/mybatis/service/EmployeeService.java
+++ src/com/stone/mybatis/service/EmployeeService.java
@@ -2,6 +2,7 @@ package com.stone.mybatis.service;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,7 +14,11 @@ public class EmployeeService {
 	@Autowired
 	private EmployeeMapper employeeMapper;
 	
+	@Autowired
+	private SqlSession sqlSession;
+	
 	public List<Employee> getEmps(){
+		//EmployeeMapper mapper = sqlSession.getMapper(EmployeeMapper.class);
 		return employeeMapper.getEmps();
 	}
 }
